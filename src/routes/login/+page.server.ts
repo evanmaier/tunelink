8ba63f62f666ab662f18e4e7d233e665ec58106d
Evan Maier <evanm413r@gmail.com>
2024-08-27 20:49:03 -0700

    add sign in functionality

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,19 +1,32 @@
 import type { Actions } from "@sveltejs/kit";
 import { redirect } from "@sveltejs/kit";
 import { auth } from "$lib/firebase";
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 
 export const actions: Actions = {
-	default: async (event) => {
-	    const formData = await event.request.formData();
+	signIn: async ({request}) => {
+	    const formData = await request.formData();
         const email = formData.get('email') as string;
         const password = formData.get('password') as string;
         try {
             const userCredential = await signInWithEmailAndPassword(auth, email, password);
             console.log(userCredential);
-            throw redirect(302, '/dashboard');
         } catch (error) {
             console.error(error);
         }
-	}
+        throw redirect(303, '/dashboard');
+	},
+
+    signUp: async ({request}) => {
+        const formData = await request.formData();
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+        try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(userCredential);
+        } catch (error) {
+            console.error(error);
+        }
+        throw redirect(303, '/dashboard');
+    }
 };
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,10 +7,11 @@
 
         <h2 class="text-2xl font-bold mb-6">Sign in</h2>
         
-        <form method="POST" class="flex flex-col gap-4 p-4">
+        <form method="POST" action="?/signIn"class="flex flex-col gap-4 p-4">
             <input name="email" type="email" placeholder="Email" class="input input-bordered w-full max-w-s">
             <input name="password" type="text" placeholder="Password" class="input input-bordered w-full max-w-s">
-            <button class="btn">Sign in </button>
+            <button formaction="?/signIn" class="btn">Sign in</button>
+            <button formaction="?/signUp" class="btn btn-primary" >Sign up</button>
         </form>
 
     </div>
