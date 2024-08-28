22ba632da1bbda1d37aaadfc265663c79654bc44
Evan Maier <evanm413r@gmail.com>
2024-08-27 17:34:15 -0700

    add sign in page

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
@@ -21,7 +20,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore();
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { auth } from "$lib/firebase";
+
+    import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+    async function signInWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        const user = await signInWithPopup(auth, provider);
+        console.log(user)
+    }
+</script>
+<div class="flex justify-center items-center min-h-screen">
+	<div class="w-full max-w-sm text-center">
+        <h2 class="text-2xl font-bold mb-6">Sign in</h2>
+
+        <form method="POST" class="flex flex-col gap-4">
+            <input name="username" type="text" placeholder="Username" class="input input-bordered w-full max-w-s">
+            <input name="password" type="text" placeholder="Password" class="input input-bordered w-full max-w-s">
+            <button class="btn">Sign in with email</button>
+            <button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
+        </form>
+        
+    </div>
+</div>
