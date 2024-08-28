f346538e93531d9a152a5ee9b6f331f113230ee6
Evan Maier <evanm413r@gmail.com>
2024-08-27 18:42:56 -0700

    add login actions

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,18 @@
+import type { Actions } from "@sveltejs/kit";
+import { redirect } from "@sveltejs/kit";
+import { auth } from "$lib/firebase";
+import { signInWithEmailAndPassword } from 'firebase/auth';
+export const actions: Actions = {
+	default: async (event) => {
+	    const formData = await event.request.formData();
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            console.log(userCredential);
+        } catch (error) {
+            console.error(error);
+        }
+        throw redirect(302, '/dashboard');
+	}
+};
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
     import { auth } from "$lib/firebase";
-
-    import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+    import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
     async function signInWithGoogle() {
-        const provider = new GoogleAuthProvider();
-        const user = await signInWithPopup(auth, provider);
-        console.log(user)
+        try {
+            const provider = new GoogleAuthProvider();
+            const user = await signInWithPopup(auth, provider);
+            console.log(user)
+        } catch (error) {
+            console.error(error);
+        }        
     }
+
 </script>
+
 <div class="flex justify-center items-center min-h-screen">
 	<div class="w-full max-w-sm text-center">
+
         <h2 class="text-2xl font-bold mb-6">Sign in</h2>
 
-        <form method="POST" class="flex flex-col gap-4">
-            <input name="username" type="text" placeholder="Username" class="input input-bordered w-full max-w-s">
+        <form method="POST" action="/login" class="flex flex-col gap-4">
+            <input name="email" type="email" placeholder="Email" class="input input-bordered w-full max-w-s">
             <input name="password" type="text" placeholder="Password" class="input input-bordered w-full max-w-s">
             <button class="btn">Sign in with email</button>
             <button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
