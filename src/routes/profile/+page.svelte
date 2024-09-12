9f48d590579d35fc3720dd7fc7901a9a4c391d41
Evan Maier <evanm413r@gmail.com>
2024-09-12 15:02:59 -0700

    Delete users from database Fixes #1

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,10 +1,43 @@
 <script lang="ts">
 	import AuthCheck from '$lib/components/AuthCheck.svelte';
+	import { db } from '$lib/firebase';
+	import { user } from '$lib/stores/AuthStore';
+	import { userData } from '$lib/stores/DocStore';
+	import { deleteUser } from 'firebase/auth';
+	import { doc, deleteDoc } from 'firebase/firestore';
+
+	async function deleteAccount() {
+		if (!$user || !$userData) {
+			console.log('user or their data is null');
+			return;
+		}
+
+		await deleteDoc(doc(db, 'usernames', $userData?.username))
+			.then(() => {
+				console.log(`username has been deleted`);
+			})
+			.catch((error) => {
+				console.log(error.message);
+			});
+
+		await deleteDoc(doc(db, 'users', $user.uid))
+			.then(() => {
+				console.log(`user data has been deleted`);
+			})
+			.catch((error) => {
+				console.log(error.message);
+			});
+
+		await deleteUser($user)
+			.then(() => {
+				console.log(`user auth has been deleted`);
+			})
+			.catch((error) => {
+				console.log(error.message);
+			});
+	}
 </script>
 
 <AuthCheck>
-	<h1>Profile</h1>
-	<p>todo: profile picture</p>
-	<p>todo: change email and password</p>
-	<p>todo: bio</p>
+	<button on:click={deleteAccount} class="btn btn-warning">Delete Account</button>
 </AuthCheck>
