b26408026081fb7621c41d7c9a177659ebc1a3bb
Evan Maier <evanm413r@gmail.com>
2024-09-19 23:01:01 -0700

    don't show instruments you own in search results Fixes #17

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { latitude, longitude } from '$lib/stores/GeoStore';
+	import { user } from '$lib/stores/AuthStore';
 	import algoliasearch from 'algoliasearch';
 	import type { SearchClient, SearchIndex } from 'algoliasearch';
 	import { goto } from '$app/navigation';
@@ -19,28 +20,36 @@
 	});
 
 	async function userSearch() {
-		await index.search(query, {
-            hitsPerPage: 3,
-            attributesToRetrieve: ['name', 'pictures', 'id'],
-        }).then(({hits})=> {
-            searchHits = hits;
-        }).catch((error)=> {
-			console.error('Error performing user search:', error);
-		});
+		await index
+			.search(query, {
+				filters: `NOT owner:${$user?.uid}`,
+				hitsPerPage: 3,
+				attributesToRetrieve: ['name', 'pictures', 'id']
+			})
+			.then(({ hits }) => {
+				searchHits = hits;
+			})
+			.catch((error) => {
+				console.error('Error performing user search:', error);
+			});
 	}
 
 	async function localSearch() {
-		await index.search('', {
-            aroundRadius: radius*1000,
-            aroundLatLng: `${$latitude}, ${$longitude}`,
-            hitsPerPage: 3,
-            attributesToRetrieve: ['name', 'pictures', 'id'],
-        }).then(({hits})=> {
-            localHits = hits;
-        }).catch((error)=> {
-			console.error('Error performing local search:', error);
-		});
-    }
+		await index
+			.search('', {
+				filters: `NOT owner:${$user?.uid}`,
+				aroundRadius: radius * 1000,
+				aroundLatLng: `${$latitude}, ${$longitude}`,
+				hitsPerPage: 3,
+				attributesToRetrieve: ['name', 'pictures', 'id']
+			})
+			.then(({ hits }) => {
+				localHits = hits;
+			})
+			.catch((error) => {
+				console.error('Error performing local search:', error);
+			});
+	}
 
 	function gotoitem(hit: any) {
 		goto(`/items/${hit.objectID}`);
@@ -69,13 +78,12 @@
 	{/each}
 </div>
 
-<h2 class='text-2xl font-bold text-center mt-10'>Local Instruments</h2>
+<h2 class="text-2xl font-bold text-center mt-10">Local Instruments</h2>
 
-<div class='flex flex-row items-center space-x-2'>
-    <label for="radius" class='label-text'>Search Radius km</label>
-    <input type="text" id='radius' bind:value={radius} on:change={localSearch} placeholder="search radius (m)" class='max-w-20'>
+<div class="flex flex-row items-center space-x-2">
+	<label for="radius" class="label-text">Search Radius km</label>
+	<input type="text" id="radius" bind:value={radius} on:change={localSearch} class="max-w-20" />
 </div>
-
 
 <div class="grid grid-flow-col auto-cols-max gap-2">
 	{#each localHits as hit}
