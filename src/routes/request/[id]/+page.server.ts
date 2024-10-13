60b6d60f164a40b7b9a5aff0c8368721d7660282
Evan Maier <evanm413r@gmail.com>
2024-10-13 12:10:53 -0700

    finished basic request page

--- src/routes/request/[id]/+page.server.ts
+++ src/routes/request/[id]/+page.server.ts
@@ -3,20 +3,31 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const requestRef = adminDB.collection('requests').doc(params.id);
-    const requestDoc = await requestRef.get();
-    const messages = await requestRef.collection('messages').orderBy('timestamp').get();
-    
-    const instrumentDoc = await adminDB
+
+	const requestDoc = await requestRef.get();
+
+	const messages = await requestRef.collection('messages').orderBy('timestamp').get();
+
+	let messageData = messages.docs.map((doc) => {
+		return {
+			message: doc.get('message'),
+			senderID: doc.get('senderID'),
+			timestamp: doc.get('timestamp').toDate()
+		};
+	});
+
+	const instrumentDoc = await adminDB
 		.collection('instruments')
 		.doc(requestDoc.get('instrumentID'))
 		.get();
 
-    let instrumentData = instrumentDoc.data();
-    delete instrumentData?.timestamp;
-
 	return {
-        instrument: instrumentData,
-        messages: messages.docs,
-
-    };
+		imageURL: instrumentDoc.get('imageURL'),
+		price: instrumentDoc.get('price'),
+		name: instrumentDoc.get('name'),
+		description: instrumentDoc.get('description'),
+		dates: requestDoc.get('Dates'),
+		requestID: requestRef.id,
+		messages: messageData
+	};
 };
--- src/routes/request/[id]/+page.svelte
+++ src/routes/request/[id]/+page.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-
+	import { user } from '$lib/stores/AuthStore';
 	export let data: PageData;
 </script>
+
+<div class="grid grid-cols-2 gap-4 p-4">
+	<div class="flex flex-row">
+		<img src={data.imageURL} class=" max-w-xl" alt="instrument" />
+		<div class="flex flex-col gap-4 p-4">
+			<p>start date: {data.dates.start}</p>
+			<p>end date: {data.dates.end}</p>
+			<p>name: {data.name}</p>
+			<p>price: ${data.price}</p>
+			<p>description: {data.description}</p>
+		</div>
+	</div>
+
+	<div class="flex flex-col border">
+		{#each data.messages as message}
+			<div class="flex flex-row gap-4">
+				<p>{message.timestamp.toISOString().split('T')[0]}</p>
+				<p class:text-left={message.senderID == $user?.uid} class="text-right">
+					{message.message}
+				</p>
+			</div>
+		{/each}
+	</div>
+</div>
