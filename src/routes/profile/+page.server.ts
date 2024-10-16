e8d58b8232a63e814d1e64765ff8ca1dee4925d2
Evan Maier <evanm413r@gmail.com>
2024-10-16 13:48:06 -0700

    Change username, password, delete account work in progress

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,5 +1,41 @@
-import type { PageServerLoad } from './$types';
+import { adminDB, adminAuth } from '$lib/server/admin';
+import type { Actions } from './$types';
+import { fail } from '@sveltejs/kit';
 
-export const load = (async () => {
-	return {};
-}) satisfies PageServerLoad;
+export const actions: Actions = {
+	username: async ({ request }) => {
+		const data = await request.formData();
+		const uid = data.get('uid') as string;
+		const username = data.get('username') as string;
+		const userRef = adminDB.collection('users').doc(uid);
+		try {
+			await userRef.update({ username });
+		} catch (error: any) {
+			return fail(400, { error: error.message });
+		}
+		return { success: true };
+	},
+
+	password: async ({ request }) => {
+		const data = await request.formData();
+		const uid = data.get('uid') as string;
+		const password = data.get('password') as string;
+		try {
+			await adminAuth.updateUser(uid, { password });
+		} catch (error: any) {
+			return fail(400, { error: error.message });
+		}
+		return { success: true };
+	},
+
+	delete: async ({ request }) => {
+		const data = await request.formData();
+		const uid = data.get('uid') as string;
+		try {
+			await adminAuth.deleteUser(uid);
+		} catch (error: any) {
+			return fail(400, { error: error.message });
+		}
+		return { success: true };
+	}
+}
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,24 +1,25 @@
 <script lang="ts">
 	import AuthCheck from '$lib/components/AuthCheck.svelte';
 	import { user } from '$lib/stores/AuthStore';
-	import { deleteUser } from 'firebase/auth';
-
-	async function deleteAccount() {
-		if (!$user) {
-			console.log('user or their data is null');
-			return;
-		}
-
-		await deleteUser($user)
-			.then(() => {
-				console.log(`user auth has been deleted`);
-			})
-			.catch((error) => {
-				console.log(error.message);
-			});
-	}
 </script>
 
 <AuthCheck>
-	<button on:click={deleteAccount} class="btn btn-warning">Delete Account</button>
+	<form action="/username" method="post">
+		<input type="hidden" value={$user?.uid}>
+		<label for="username">New Username</label>
+		<input type="text" id="username" required>
+		<button class="btn btn-primary">Update Username</button>
+	</form>
+
+	<form action="/password" method="post">
+		<input type="hidden" value={$user?.uid}>
+		<label for="password">New Password</label>
+		<input type="text" id="password" required>
+		<button class="btn btn-primary">Update Password</button>
+	</form>
+
+	<form action="/delete" method="post">
+		<input type="hidden" value={$user?.uid}>
+		<button class="btn btn-warning">Delete Account</button>
+	</form>
 </AuthCheck>
