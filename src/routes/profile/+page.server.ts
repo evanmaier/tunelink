6703fec7a0be9175ede96b740af340cb9a867da6
Evan Maier <evanm413r@gmail.com>
2024-10-17 10:28:20 -0700

    allow user to change their username and password

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,41 +1,45 @@
-import { adminDB, adminAuth } from '$lib/server/admin';
+import { adminAuth } from '$lib/server/admin';
 import type { Actions } from './$types';
 import { fail } from '@sveltejs/kit';
 
 export const actions: Actions = {
-	username: async ({ request }) => {
-		const data = await request.formData();
-		const uid = data.get('uid') as string;
-		const username = data.get('username') as string;
-		const userRef = adminDB.collection('users').doc(uid);
-		try {
-			await userRef.update({ username });
-		} catch (error: any) {
-			return fail(400, { error: error.message });
+	username: async ({ request, locals }) => {
+		if (locals.userID) {
+			const data = await request.formData();
+			const username = data.get('username') as string;
+			try {
+				await adminAuth.updateUser(locals.userID, { displayName: username });
+			} catch (error: any) {
+				return fail(400, { error: error.message });
+			}
+			return { success: true };
 		}
-		return { success: true };
+		return fail(400, { error: 'missing userID' });
 	},
 
-	password: async ({ request }) => {
-		const data = await request.formData();
-		const uid = data.get('uid') as string;
-		const password = data.get('password') as string;
-		try {
-			await adminAuth.updateUser(uid, { password });
-		} catch (error: any) {
-			return fail(400, { error: error.message });
+	password: async ({ request, locals }) => {
+		if (locals.userID) {
+			const data = await request.formData();
+			const password = data.get('password') as string;
+			try {
+				await adminAuth.updateUser(locals.userID, { password });
+			} catch (error: any) {
+				return fail(400, { error: error.message });
+			}
+			return { success: true };
 		}
-		return { success: true };
+		return fail(400, { error: 'missing userID' });
 	},
 
-	delete: async ({ request }) => {
-		const data = await request.formData();
-		const uid = data.get('uid') as string;
-		try {
-			await adminAuth.deleteUser(uid);
-		} catch (error: any) {
-			return fail(400, { error: error.message });
+	delete: async ({ locals }) => {
+		if (locals.userID) {
+			try {
+				await adminAuth.deleteUser(locals.userID);
+			} catch (error: any) {
+				return fail(400, { error: error.message });
+			}
+			return { success: true };
 		}
-		return { success: true };
+		return fail(400, { error: 'missing userID' });
 	}
-}
+};
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,25 +1,25 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import AuthCheck from '$lib/components/AuthCheck.svelte';
-	import { user } from '$lib/stores/AuthStore';
 </script>
 
 <AuthCheck>
-	<form action="/username" method="post">
-		<input type="hidden" value={$user?.uid}>
-		<label for="username">New Username</label>
-		<input type="text" id="username" required>
-		<button class="btn btn-primary">Update Username</button>
-	</form>
+	<h2 class="text-2xl font-bold text-center m-10">Profile</h2>
+	<div class="flex flex-col items-center">
+		<form action="?/username" method="post" class="flex flex-col p-2 space-y-2" use:enhance>
+			<label for="username">New Username</label>
+			<input type="text" id="username" class="input input-bordered w-full max-w-sm" required />
+			<button class="btn btn-primary">Update Username</button>
+		</form>
 
-	<form action="/password" method="post">
-		<input type="hidden" value={$user?.uid}>
-		<label for="password">New Password</label>
-		<input type="text" id="password" required>
-		<button class="btn btn-primary">Update Password</button>
-	</form>
+		<form action="?/password" method="post" class="flex flex-col p-2 space-y-2" use:enhance>
+			<label for="password">New Password</label>
+			<input type="text" id="password" class="input input-bordered" required />
+			<button class="btn btn-primary w-full max-w-sm">Update Password</button>
+		</form>
 
-	<form action="/delete" method="post">
-		<input type="hidden" value={$user?.uid}>
-		<button class="btn btn-warning">Delete Account</button>
-	</form>
+		<form action="?/delete" method="post" class="p-2 flex w-[230px]" use:enhance>
+			<button class="btn btn-warning w-full">Delete Account</button>
+		</form>
+	</div>
 </AuthCheck>
