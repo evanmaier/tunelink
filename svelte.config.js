a6bfa443df851a7cf24f514a5677dbc5507679a9
Evan Maier <evanm413r@gmail.com>
2024-11-01 19:10:20 -0700

    prepare for vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
