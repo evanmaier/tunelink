08830a80ffda73fe94cbc6425246122b7cdca1ef
Evan Maier <evanm413r@gmail.com>
2024-11-12 11:59:44 -0800

    fix typos in about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,33 +9,33 @@
 		</p>
 		<h2>How it works</h2>
 		<p>
-			There are two ways to use the application. Either as a renter, or as a owner/lender. all users
+			There are two ways to use the application. Either as a renter or as an owner/lender. all users
 			can do both.
 		</p>
 		<h3>Renting</h3>
 		<p>
 			First, you can search by keyword or browse the local instruments selection to find what you
-			need. Next you send a rental request by navigating to the item page and clicking the request
-			rental buttonm, then filling out the form. To do this, you need to have an account set up with
+			need. Next, you send a rental request by navigating to the item page, clicking the request
+			rental button, and submitting the form. To do this, you need to have an account set up with
 			a username, email, and password. Once the request is sent, you can track it's status on the
 			rentals page. You can also chat with the owner to arrange pickup and payment.
 		</p>
 		<h3>Lending</h3>
 		<p>
-			Users can add an instrument by navigating to the add instrument page and filling out the form.
-			Once uploaded, you can edit the details using the your instruments page. Once this is
-			complete, other users can see their listing and send rental requests. If you receive a rental
+			Users can add an instrument by navigating to the Add Instrument page and filling out the form.
+			Once uploaded, you can edit the details using the Your Instruments page. Once this is
+			complete, other users can see their listings and send rental requests. If you receive a rental
 			request, it will show up in the pending section on the rentals page. You can accept or reject
 			this rental, and chat with the renter. Once the request is accepted, It moves to the accepted
 			section. When the renter collects the instrument, you can activate the rental, which marks
 			your instrument as unavailable to other users. Finally, when the instrument is returned, you
-			can mark the rental as complete which.
+			can mark the rental as complete.
 		</p>
-		<h2>Buisiness model</h2>
+		<h2>Business model</h2>
 		<p>
-			Currently this project is in alpha testing, the goal of which is to collect feedback and find
-			bugs. Once I feel like the product is ready, we will add a free tier and paid tier. The plan
-			is to allow users/buisinesses to list a small number of instruments for free, and charge a
+			Currently, this project is in alpha testing, the goal of which is to collect feedback and find
+			bugs. Once I feel like the product is ready, we will add a free tier and a paid tier. The plan
+			is to allow users/businesses to list a small number of instruments for free, and charge a
 			monthly fee for more listings. Our target customers are music stores with rental programs and
 			a small online presence, and local musicians with lots of gear.
 		</p>
